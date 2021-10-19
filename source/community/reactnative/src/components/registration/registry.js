3ac4fe25c84cdada8ef12a4fb10c40b53387ef25
Supreet Singh <supsing@microsoft.com>
2021-10-19 15:37:50 +0530

    Added factset/text as well.

--- source/community/reactnative/src/components/registration/registry.js
+++ source/community/reactnative/src/components/registration/registry.js
@@ -181,7 +181,8 @@ export class Registry {
 				}
 				if (isValid) {
 					if (element.isVisible !== false) {
-						const addIdToKey = element.type !== 'Image' && element.type !== 'Icon' && element.type !== 'Container' && element.type !== 'Column' && element.type !== 'ColumnSet' && element.type !== 'ImageSet';
+						const addIdToKey = element.type !== 'Image' && element.type !== 'Icon' && element.type !== 'Container' && element.type !== 'Column' && element.type !== 'ColumnSet'
+							&& element.type !== 'ImageSet' && element.type !== 'TextBlock' && element.type !== 'FactSet';
 						const elementKey = Utils.isNullOrEmpty(element.id) ? `${element.type}-${index}` : addIdToKey ? `${element.type}-${index}-${element.id}` : `${element.type}-${index}`;
 						
 						return (<Element json={element} key={elementKey} />);
